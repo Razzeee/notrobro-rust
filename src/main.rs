b2b65948f6e0c6cf1e940f16281852f601f82775
Tobias Veldboer <t.veldboer@salt-and-pepper.eu>
2018-09-14 13:21:18 +0200

    a bit nicer to read

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,36 @@ use std::path::{Path, PathBuf};
 extern crate walkdir;
 use walkdir::WalkDir;
 extern crate clap;
-use clap::{Arg, App};
+use clap::{App, Arg};
 
 fn main() {
-   let matches = App::new("Notrobro")
-                          .version("1.0")
-                          .author("Team Kodi")
-                          .about("Finds intros and outros. Then creates files, so your videoplayers can skip those.")
-                          .arg(Arg::with_name("PATH")
-                               .short("p")
-                               .long("path")
-                               .value_name("PATH")
-                               .help("TV show directory path (mandatory argument)")
-                               .required(true)
-                               .takes_value(true))
-                          .arg(Arg::with_name("THRESHOLD")
-                               .short("t")
-                               .long("threshold")
-                               .help("Threshold for scene change detection(default=0.35)")
-                               .takes_value(true))
-                          .arg(Arg::with_name("FORCE")
-                               .short("f")
-                               .long("force")
-                               .help("Process all videos in the directory (default=False)"))
-                          .get_matches();
+    let matches = App::new("Notrobro")
+        .version("1.0")
+        .author("Team Kodi")
+        .about("Finds intros and outros. Then creates files, so your videoplayers can skip those.")
+        .arg(
+            Arg::with_name("PATH")
+                .short("p")
+                .long("path")
+                .value_name("PATH")
+                .help("TV show directory path (mandatory argument)")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("THRESHOLD")
+                .short("t")
+                .long("threshold")
+                .help("Threshold for scene change detection(default=0.35)")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("FORCE")
+                .short("f")
+                .long("force")
+                .help("Process all videos in the directory (default=False)"),
+        )
+        .get_matches();
 
     let path_string = matches.value_of("PATH").unwrap();
     let threshold = matches.value_of("THRESHOLD").unwrap_or("0.35");
@@ -43,42 +49,40 @@ fn main() {
     if path.exists() {
         println!("Path exists");
 
-        for entry in WalkDir::new(path_string).into_iter().filter_map(|e| e.ok()) {
-            match entry.path().extension() {
-                Some(extension) => 
-                    match extension.to_str() {
-                        Some("mp4")|Some("mkv")|Some("avi")|Some("mov")|Some("wmv")  => {
-                            println!("{}", entry.path().display());
-                             match entry.path().file_stem() {
-                                 Some(_) => {
-                                     let mut p = PathBuf::from(entry.path());
-                                     p.set_extension("edl");
-                                     let edl_path = Path::new(&p);
-                                     match edl_path.exists() {
-                                         true => {
-                                             println!("Edl does exist {}", edl_path.display());
-                                                if force {
-                                                    // Proceed
-                                                }
-                                             },
-                                         false => {
-                                             println!("Edl does not exist {}", edl_path.display());
-                                         }
-                                     }
-                                     },
-                                 None => ()
-                             }
-                        },
-                        _ => (),
-                    },
-                None => ()
-            }
-        }
-
-
+        WalkDir::new(path_string)
+            .into_iter()
+            .filter_map(|e| e.ok())
+            .for_each(|e| {
+                do_something(e.path(), force);
+            });
     } else {
         println!("Path doesn't seem to exist. Did you mistype?");
     }
-
 }
 
+fn do_something(path: &Path, force: bool) -> Option<()> {
+    match path.extension()?.to_str()? {
+        "mp4" | "mkv" | "avi" | "mov" | "wmv" => {
+            println!("{}", path.display());
+
+            if path.file_stem().is_some() {
+                let mut edl_path: PathBuf = path.into();
+                edl_path.set_extension("edl");
+
+                if edl_path.exists() {
+                    println!("Edl does exist {}", edl_path.display());
+
+                    if force {
+                        // Proceed
+                    }
+                } else {
+                    println!("Edl does not exist {}", edl_path.display());
+                }
+            }
+        }
+
+        _ => (),
+    }
+
+    Some(())
+}
