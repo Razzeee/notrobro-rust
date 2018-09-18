9d47bd79fe32ca88e2cd14f38c4fcd72f85582f6
Tobias Veldboer <t.veldboer@salt-and-pepper.eu>
2018-09-18 17:28:22 +0200

    nicer crawling

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,11 @@ use walkdir::WalkDir;
 extern crate clap;
 use clap::{App, Arg};
 
+struct Folder {
+    folder_path: PathBuf,
+    video_files: Vec<PathBuf>,
+}
+
 fn main() {
     let matches = App::new("Notrobro")
         .version("1.0")
@@ -18,21 +23,18 @@ fn main() {
                 .help("TV show directory path (mandatory argument)")
                 .required(true)
                 .takes_value(true),
-        )
-        .arg(
+        ).arg(
             Arg::with_name("THRESHOLD")
                 .short("t")
                 .long("threshold")
                 .help("Threshold for scene change detection(default=0.35)")
                 .takes_value(true),
-        )
-        .arg(
+        ).arg(
             Arg::with_name("FORCE")
                 .short("f")
                 .long("force")
                 .help("Process all videos in the directory (default=False)"),
-        )
-        .get_matches();
+        ).get_matches();
 
     let path_string = matches.value_of("PATH").unwrap();
     let threshold = matches.value_of("THRESHOLD").unwrap_or("0.35");
@@ -45,39 +47,60 @@ fn main() {
     println!("Using force: {}", force);
 
     let path = Path::new(path_string);
+    let mut folder_count = 0;
 
     if path.exists() {
         println!("Path exists");
 
-        WalkDir::new(path_string)
+        // find all folders with two or more video files
+        let folders: Vec<Folder> = WalkDir::new(path_string)
             .into_iter()
-            .filter_map(| e | e.ok() )
-            .filter( | e | {
+            .filter_map(|result| result.ok())
+            .filter(|entry| entry.file_type().is_dir())
+            .map(|folder| {
+                
+                // find all videos in the folder
+                folder_count += 1;
 
-                let ext = e.path().extension().and_then( | s | s.to_str() );
+                let videos: Vec<PathBuf> = WalkDir::new(folder.path())
+                    .max_depth(1)
+                    .into_iter()
+                    .filter_map(|result| result.ok())
+                    .filter(|entry| entry.is_video())
+                    .map(|entry| entry.path().into())
+                    .collect();
 
-                if let Some(ext) = ext {
-                    match ext {
-                        "mp4" | "mkv" | "avi" | "mov" | "wmv" => {
-                            println!("{}", path.display());
-                            true
-                        }
-                        _ => false
-                    }
+                Folder {
+                    folder_path: folder.path().into(),
+                    video_files: videos,
                 }
-                else { false }
+            }).filter(|folder| folder.video_files.len() >= 2)
+            .collect();
 
-            } )
-            .for_each(| e | {
-                get_edl(e.path());
-            });
+        println!(
+            "{} folders found, {} folders searched",
+            folders.len(),
+            folder_count
+        );
+
+        // 1. create edl-files
+        for folder in folders {
+            folder
+                .video_files
+                .into_iter()
+                .filter(|video| force || !video.has_edl())
+                .for_each(|video| println!("creating edl for {}", video.display()))
+        }
+
+    // 2. compare edls ???
+
+    // 3. Profit!
     } else {
         println!("Path doesn't seem to exist. Did you mistype?");
     }
 }
 
 fn get_edl(path: &Path) -> Option<PathBuf> {
-
     if path.file_stem().is_some() {
         let mut edl_path: PathBuf = path.into();
         edl_path.set_extension("edl");
@@ -89,6 +112,81 @@ fn get_edl(path: &Path) -> Option<PathBuf> {
             println!("Edl does not exist {}", edl_path.display());
             None
         }
+    } else {
+        None
     }
-    else { None }
+}
+
+trait Notrobro {
+    fn is_video(&self) -> bool;
+    fn has_edl(&self) -> bool;
+}
+
+impl Notrobro for Path {
+    fn is_video(&self) -> bool {
+        let ext = self.extension().and_then(|s| s.to_str());
+
+        if let Some(ext) = ext {
+            match ext {
+                "mp4" | "mkv" | "avi" | "mov" | "wmv" => true,
+                _ => false,
+            }
+        } else {
+            false
+        }
+    }
+
+    fn has_edl(&self) -> bool {
+        if self.file_stem().is_some() {
+            let mut edl_path: PathBuf = self.into();
+            edl_path.set_extension("edl");
+
+            if edl_path.exists() {
+                true
+            } else {
+                false
+            }
+        } else {
+            false
+        }
+    }
+}
+
+impl Notrobro for walkdir::DirEntry {
+    fn is_video(&self) -> bool {
+        self.path().is_video()
+    }
+    fn has_edl(&self) -> bool {
+        self.path().has_edl()
+    }
+}
+
+impl Notrobro for PathBuf {
+    fn is_video(&self) -> bool {
+        let ext = self.extension().and_then(|s| s.to_str());
+
+        if let Some(ext) = ext {
+            match ext {
+                "mp4" | "mkv" | "avi" | "mov" | "wmv" => true,
+                _ => false,
+            }
+        } else {
+            false
+        }
+    }
+
+    fn has_edl(&self) -> bool {
+        if self.file_stem().is_some() {
+            let mut edl_path: PathBuf = self.into();
+            edl_path.set_extension("edl");
+
+            if edl_path.exists() {
+                true
+            } else {
+                false
+            }
+        } else {
+            false
+        }
+    }
 }
